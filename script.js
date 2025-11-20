13e6d162ab832134f7b6bf94402e29caf7dcedb8
Adamu Safiya Iman <imansafiya2006@gmail.com>
2025-11-21 00:09:24 +0100

    i used \\ instead of ||

--- script.js
+++ script.js
@@ -6,5 +6,12 @@ const guessInput = document.getElementById("guessInput");
 const submitBtn = document .getElementById("submitBtn");
 const feedback =document .getElementById("feedback");
 const attemptsDisplay =document .getElementById("attempts");
-const resetBtn =document .getElementById("resetBtn")
+const resetBtn =document .getElementById("resetBtn");
 
+
+function checkGuess() {
+    const userGuess = Number(guessInput.value);
+    if(!userGuess =|| < 1 || userguess > 50) {
+
+    }
+}
