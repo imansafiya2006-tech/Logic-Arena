472e2c299f62a934fc493473d010215341684ec4
Adamu Safiya Iman <imansafiya2006@gmail.com>
2025-11-20 23:55:57 +0100

    included the double quotes on attempts

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 let randomNumber =Math.floor(Math.random() * 50) + 1;
 let attempts = 0;
 const maxAttempts = 3;
+
+const guessInput = document.getElementById("guessInput");
+const submitBtn = document .getElementById("submitBtn");
+const feedback =document .getElementById("feedback");
+const attemptsDisplay =document .getElementById("attempts");
+
